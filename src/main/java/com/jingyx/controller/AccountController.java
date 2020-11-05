fbb333af1203946e089c378f89f9c88e82a26da8
jingyx <snow_0506@163.com>
2020-11-06 00:52:00 +0800

    commit upload function

--- src/main/java/com/jingyx/controller/AccountController.java
+++ src/main/java/com/jingyx/controller/AccountController.java
@@ -8,15 +8,24 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@Api("用户账单api")
+@Api(tags={"用户账单控制器"})
 @RestController
-@RequestMapping("/account")
+@RequestMapping("account")
 @Slf4j
 public class AccountController {
+
+	@Value("${template.file}")
+	private String filePath;
 
 	@Autowired
 	IAccountService accountService;
@@ -62,6 +71,76 @@ public class AccountController {
 	public ReturnMsg getAccountList(@RequestParam("pageNum") Integer pageNum, @RequestParam("pageSize") Integer pageSize){
 		PageInfo<Account> accountList = accountService.getAccountList(pageNum, pageSize);
 		return new ReturnMsg(200, "查询账单信息成功", accountList);
+	}
+
+	@PostMapping("upload")
+	@ApiOperation(value="模板上传")
+	public ReturnMsg upload(MultipartFile file) {
+		//MultipartFile对象的名称必须和html中的文件上传标签的名字相同
+		if (file.getSize() == 0) return new ReturnMsg(400, "不能上传空文件");
+		if (!"txt".equals(file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1)))
+			return new ReturnMsg(400, "请上传txt格式文件");
+		try {
+			String filename = file.getOriginalFilename();
+			File targetPath = new File(filePath);
+			if (!targetPath.exists()) targetPath.mkdir();
+			String pathName = filePath + filename;
+			File targetFile = new File(pathName);
+			file.transferTo(targetFile);
+			Account account = getAccountEntityFromTxt(targetFile);
+			accountService.addAccount(account);
+		} catch (Exception e) {
+			log.error("文件内容不符合标准，请核查");
+			return new ReturnMsg(500, "文件内容不符合标准，请核查");
+		}
+		return new ReturnMsg(200, "上传成功");
+	}
+
+	/**
+	 * 功能描述: <br>
+	 * 〈把模板文件信息转成用户账单对象〉
+	 * @Param: [file]
+	 * @Return: com.jingyx.entity.Account
+	 * @Author: Jingyx
+	 * @Date: 2020/11/5 17:36
+	 */
+	private Account getAccountEntityFromTxt(File file) {
+		// 字符输入流进行读取操作读取
+		BufferedReader bufferedReader = null;
+		// 每一行的内容
+		String tempString;
+		// 返回的密钥模板
+		Account account = null;
+		// 行号
+		int line =1;
+		try {
+			// 输入字节流，FileInputStream主要用来操作文件输入流
+			FileInputStream fileInputStream = new FileInputStream(file);
+			// InputStreamReader是转换流，将字节流转成字符流
+			bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream, "UTF-8"));
+			Map<Integer, String> map = new HashMap();
+			while ((tempString = bufferedReader.readLine()) != null) {
+				map.put(line, tempString);
+				line++;
+			}
+			account = new Account();
+			account.setAccountNum(map.get(1));
+			account.setAccountType(Byte.parseByte(map.get(2)));
+			account.setQuota(Float.parseFloat(map.get(3)));account.setCostType(Byte.parseByte(map.get(4)));
+			account.setRemark(map.get(5));
+			account.setCreateTime(new Date());
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return account;
 	}
 
 }
